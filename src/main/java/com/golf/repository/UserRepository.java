cb2c3efd676a794a5c1a0efb17fc8dbfb56b7644
Julian-Val <julianval1710@gmail.com>
2025-11-12 21:04:39 -0800

    added user entity, repository, controller, and service. As well as security config for password hashing. Tested using curl.

---
+++ src/main/java/com/golf/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.golf.repository;
+
+import com.golf.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    Optional<User> findByUsername(String username);
+}
